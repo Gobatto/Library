1efbde618a7b10a7da0708f59f048b45dd4350f7
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-22 17:55:46 -0300

    Update library.js
    books added

--- library.js
+++ library.js
@@ -1,11 +1,11 @@
 class Media {
     constructor(title) {
-      this._title = title;
-      this._isCheckedOut = false;
-      this._ratings = [];
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
     }
     get title() {
-      return this._title;
+        return this._title;
     }
     get isCheckedOut() {
       return this._isCheckedOut;
@@ -27,6 +27,20 @@ class Media {
     }
     addRating(value) {
       this.ratings.push(value);
+    }
+}
+  
+  class Book extends Media {
+    constructor(author, title, pages) {
+      super(title);
+      this._author = author;
+      this.pages = pages;
+    }
+    get author() {
+      return this._author;
+    }
+    get pages() {
+      return this._pages;
     }
   }
   
